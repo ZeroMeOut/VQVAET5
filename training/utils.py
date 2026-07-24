6691fcf30b2aa5a54538a975953bde67f1c0a2c6
ZeroMeOut <wiziset@gmail.com>
2026-07-24 23:50:51 +0100

    Fixing for normilization issues

--- training/utils.py
+++ training/utils.py
@@ -11,6 +11,7 @@ def default_transform(image_size: int = 256):
     return transforms.Compose([
         transforms.Resize((image_size, image_size)),
         transforms.ToTensor(),
+        transforms.Normalize([0.5]*3, [0.5]*3),
     ])
 
 
